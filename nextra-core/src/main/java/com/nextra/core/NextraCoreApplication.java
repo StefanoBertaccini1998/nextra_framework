4ff69179cfe61690909291c9cf4933076d07cc73
2_S_B <s.bertacciniDev@gmail.com>
2025-10-18 08:01:17 +0200

    Fixed clean install problem parent dependecies

---
+++ nextra-core/src/main/java/com/nextra/core/NextraCoreApplication.java
@@ -0,0 +1,11 @@
+package com.nextra.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextraCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NextraCoreApplication.class, args);
+    }
+}
